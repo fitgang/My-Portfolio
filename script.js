5eb13ed764001077d19250382d62ca75cc0bd5fc
Ayush Gupta <70760483+fitgang@users.noreply.github.com>
2021-07-13 10:16:53 +0530

    added the learning section

--- script.js
+++ script.js
@@ -27,22 +27,54 @@ observer.observe(document.querySelector('#about-me'));
 
 // INTERACTIVITY WITH THE CUBE  
 
-// aligns the cube to show the required element
-function alignCube(index) {
+// DOM elements and stats
+const area = document.getElementById("portfolio");
+let grab = false;
+
+// set the value of transform property
+function transformCube(x, y) {
+    let z = 150;
+    if (innerWidth < 700) z = 100;
     const cube = document.querySelector(".projects-cube"); // the cube
-    const className = `project-${index}`; // css class to align the cube
-    if (cube.classList.contains(className)) return;
-    const projects = document.querySelectorAll(".projects");
-
-    // removing useless classes
-    for (let i = 0; i < projects.length; i++) {
-        if (cube.classList.contains(`project-${i}`)) cube.classList.remove(`project-${i}`);
-    }
-
-    // adding the required class to the cube
-    cube.classList.add(className);
+    cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) translateZ(${z}px)`;
 }
 
+// rotates the cube with the mouse
+function rotate(event) {
+    if (grab) return; // if the user is navigating through the given numbers return
+
+    // gets position of cursor
+    let x = event.clientX,
+        y = event.clientY;
+
+    // divide the area per degree
+    const dx = Math.round(area.clientWidth / (360 * 2) * 100) / 100,
+        dy = Math.round(area.clientHeight / 360 * 100) / 100;
+
+    transformCube(y * dy, x * dx);
+}
+
+
+// aligns the cube to show the required element
+function alignCube(index) {
+    grab = true;
+    const degArray = [
+        [-5, 175],
+        [85, 0],
+        [265, 0],
+        [-5, -95],
+        [175, -85],
+        [175, 185]
+    ];
+    transformCube(degArray[index][0], degArray[index][1]);
+
+    // show the description
+    const p = document.querySelectorAll(".projects")[index].querySelector(".description"); // <p> element
+    p.classList.add("show-description");
+}
+
+// EVENT LISTENERS
+area.addEventListener("mousemove", rotate);
 const nums = document.querySelectorAll(".serial-num");
 nums.forEach((n, i) => {
     n.addEventListener("click", () => alignCube(i));
